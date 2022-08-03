8b2065119d896db3cbb3290704a2e04612b7a06e
sunshay <sunshayina@outlook.com>
2022-08-03 14:29:55 -0700

    back

---
+++ ecko.js
@@ -0,0 +1,48 @@
+// var a=[1,2,3]
+// var b=[4,5,6]
+// var c= [
+// {x :1, y: 2},
+// {x :4, y: 6},
+// ];
+
+// console.log(a==b) // compare value
+// console.log(a=b) // assign value
+// console.log(a===b) // compare type
+
+// alert(c[1].x);
+
+// var str = "mike"
+// var numb = 5
+
+// function start() {
+//     for(i=0;i<5;i++ ){
+//         if(true){
+//             var color = 'black'
+//         }
+//     }
+//     console.log(color)
+// }
+// start();
+
+var obj = {
+    x: 'interview',
+    y: 5,
+    z: function question() {
+       console.log(this.y)
+    }
+}
+
+obj.z();
+
+class student{
+
+    construct(name,lastname,middlename) {
+        this.name=names
+        this.lastname=lname
+        this.middlename=mdlnm
+    }
+}
+getdetail(){
+    return Name: ${this.name}; this.lastname=lname, this.middlename=mdlnm
+}
+let student = new student (mike,mawezi,pili)
